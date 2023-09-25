29ff5d19c107e3643ce48657b2cc2998f52885cc
Oliver Säfström <safstrom.oliver@gmail.com>
2023-09-25 08:39:02 +0200

    fix warnings

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -191,6 +191,13 @@ local plugins = {
     event = "VeryLazy",
     opts = {
       -- add any options here
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
     },
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
