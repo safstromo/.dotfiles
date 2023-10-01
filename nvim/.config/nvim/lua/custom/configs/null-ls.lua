c4155a9e34fc643b746b012599a0f304c832ab54
Oliver Säfström <safstrom.oliver@gmail.com>
2023-10-01 13:50:30 +0200

    add go support

--- nvim/.config/nvim/lua/custom/configs/null-ls.lua
+++ nvim/.config/nvim/lua/custom/configs/null-ls.lua
@@ -1,31 +1,53 @@
 local null_ls = require("null-ls")
 
 local b = null_ls.builtins
-
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local sources = {
 
-  -- webdev stuff
-  -- b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier, --.with { filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css", "scss", "less", "html", "json", "jsonc", "yaml", "markdown", "markdown.mdx", "graphql", "handlebars" } }, -- so prettier works only on these filetypes
-  -- b.diagnostics.eslint,
+	-- webdev stuff
+	-- b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+	b.formatting.prettier, --.with { filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css", "scss", "less", "html", "json", "jsonc", "yaml", "markdown", "markdown.mdx", "graphql", "handlebars" } }, -- so prettier works only on these filetypes
+	-- b.diagnostics.eslint,
 
-  -- Lua
-  b.formatting.stylua,
+	-- Lua
+	b.formatting.stylua,
 
-  -- cpp
-  b.formatting.clang_format,
+	-- cpp
+	b.formatting.clang_format,
 
-  --Java
-  b.formatting.google_java_format,
+	--Java
+	b.formatting.google_java_format,
 
-  -- Diagnostics
-  b.diagnostics.eslint,
+	-- Go
+	b.formatting.gofumpt,
+	b.formatting.goimports_reviser,
+	b.formatting.golines,
 
-  -- Code actions
-  b.code_actions.eslint,
+	-- Diagnostics
+	b.diagnostics.eslint,
+
+	-- Code actions
+	b.code_actions.eslint,
 }
 
 null_ls.setup({
-  debug = true,
-  sources = sources,
+	debug = true,
+	sources = sources,
+
+	-- Format on save
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({
+				group = augroup,
+				buffer = bufnr,
+			})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
--- nvim/.config/nvim/lua/custom/configs/overrides.lua
+++ nvim/.config/nvim/lua/custom/configs/overrides.lua
@@ -1,80 +1,87 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "vim",
-    "lua",
-    "regex",
-    "bash",
-    "html",
-    "css",
-    "javascript",
-    "typescript",
-    "tsx",
-    "c",
-    "markdown",
-    "markdown_inline",
-    "java",
-    "rust",
-    "vue",
-  },
-  indent = {
-    enable = true,
-    -- disable = {
-    --   "python"
-    -- },
-  },
-  }
+	ensure_installed = {
+		"vim",
+		"lua",
+		"regex",
+		"bash",
+		"html",
+		"css",
+		"javascript",
+		"typescript",
+		"tsx",
+		"c",
+		"markdown",
+		"markdown_inline",
+		"java",
+		"rust",
+		"vue",
+		"go",
+	},
+	indent = {
+		enable = true,
+		-- disable = {
+		--   "python"
+		-- },
+	},
+}
 
 M.mason = {
-  ensure_installed = {
-    -- lua stuff
-    "lua-language-server",
-    "stylua",
+	ensure_installed = {
+		-- lua stuff
+		"lua-language-server",
+		"stylua",
 
-    -- web dev stuff
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "prettier",
-    "tailwindcss-language-server",
-    "eslint-lsp",
+		-- web dev stuff
+		"css-lsp",
+		"html-lsp",
+		"typescript-language-server",
+		"prettier",
+		"tailwindcss-language-server",
+		"eslint-lsp",
 
-    -- c/cpp stuff
-    "clangd",
-    "clang-format",
+		-- c/cpp stuff
+		"clangd",
+		"clang-format",
 
-    --java
-    "jdtls",
-    "java-test",
-    "java-debug-adapter",
-    "vscode-java-decompiler",
-    "google-java-format",
+		--java
+		"jdtls",
+		"java-test",
+		"java-debug-adapter",
+		"vscode-java-decompiler",
+		"google-java-format",
 
-    --rust
-    "rust-analyzer",
-  },
+		--rust
+		"rust-analyzer",
+
+		-- go
+		"gopls",
+    "gofumpt",
+    "goimports-reviser",
+    "golines",
+	},
 }
 
 -- git support in nvimtree
 M.nvimtree = {
-  git = {
-    enable = true,
-  },
+	git = {
+		enable = true,
+	},
 
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
 }
 
 M.telescope = {
-  extensions_list = {
-    "ui-select",
-  },
+	extensions_list = {
+		"ui-select",
+	},
 }
 return M
