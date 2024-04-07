63711e9f3153028f57bfb9d1c96ccf79eac759cb
Oliver Säfström <safstrom.oliver@gmail.com>
2024-04-07 14:55:57 +0200

    remove xserver

--- configuration.nix
+++ configuration.nix
@@ -22,6 +22,8 @@
   #Enable Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  security.polkit.enable = true;
+
   #Use latest kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
@@ -51,7 +53,7 @@
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
 
   # #Enable hyprland
   programs.hyprland = {
@@ -88,7 +90,7 @@
 
   # Enable the GNOME Desktop Environment.
   # services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.displayManager.sddm.enable = true;
   # services.xserver.desktopManager.gnome.enable = true;
 
 
