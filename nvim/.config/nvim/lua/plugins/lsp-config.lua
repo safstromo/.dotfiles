877b61a26cddf5644f2867bb3a07ecae47fe6330
Oliver Säfström <safstrom.oliver@gmail.com>
2024-05-15 12:43:21 +0200

    add diagnosics border

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -202,6 +202,10 @@ return {
 			--Set border for floating windows
 			vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]])
 			vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]])
+
+			vim.diagnostic.config({
+				float = { border = "rounded" },
+			})
 		end,
 	},
 }
