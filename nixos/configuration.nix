fe2c2d9ab95dbec3ae052391718b8aa8f0578a9a
Oliver Säfström <safstrom.oliver@gmail.com>
2025-05-07 09:43:01 +0200

    add gc

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -207,4 +207,9 @@
   # Optional, hint electron apps to use wayland:
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
 }
